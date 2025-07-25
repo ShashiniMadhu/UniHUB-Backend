a8b6248b2d97e44cfa42f5b92c23d7829ae962a3
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-07-26 02:42:16 +0530

    minor modification

--- src/main/java/com/UniHUB/Server/service/AdminService.java
+++ src/main/java/com/UniHUB/Server/service/AdminService.java
@@ -3,5 +3,11 @@ package com.UniHUB.Server.service;
 import com.UniHUB.Server.dto.UserDTO;
 
 public interface AdminService {
+
+    /**
+     * create users(Lecturer/Student)
+     * @param userDTO
+     * @return
+     */
     UserDTO createUser(UserDTO userDTO);
 }
