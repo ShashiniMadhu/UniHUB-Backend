fa7e229a2d67b465b1ed09b4712df49c601b2cab
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-07-28 02:03:06 +0530

    update feedback part and announcement  part for lecturer

--- src/main/java/com/UniHUB/Server/service/LecturerService.java
+++ src/main/java/com/UniHUB/Server/service/LecturerService.java
@@ -2,10 +2,19 @@ package com.UniHUB.Server.service;
 
 import com.UniHUB.Server.dto.AnnouncementDTO;
 import com.UniHUB.Server.dto.AssignmentsDTO;
+import com.UniHUB.Server.dto.FeedbackDTO;
 import com.UniHUB.Server.dto.ResourceDTO;
+
+import java.util.List;
 
 public interface LecturerService {
     AnnouncementDTO publishAnnouncement(AnnouncementDTO announcementDTO);
     AssignmentsDTO publishAssignment(AssignmentsDTO assignmentsDTO);
     ResourceDTO publishresources(ResourceDTO resourceDTO);
+    List<FeedbackDTO> getFeedbackForLecturer(Integer lecturerId);
+    List<AnnouncementDTO> getAnnouncementsByLecturerId(Integer lecturerId);
+    AnnouncementDTO updateAnnouncement(AnnouncementDTO announcementDTO);
+    void deleteAnnouncement(Integer announcementId);
+
+
 }
