bf9ba97a1369eb00255321851491b6bf44236d9c
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-08-18 14:21:38 +0530

    environment changed

--- src/main/java/com/UniHUB/Server/ServerApplication.java
+++ src/main/java/com/UniHUB/Server/ServerApplication.java
@@ -4,17 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
-
 @SpringBootApplication
 @RequestMapping
 public class ServerApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ServerApplication.class, args);
-
-
 	}
 }
